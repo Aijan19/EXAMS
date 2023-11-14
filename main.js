16f314adcdb3116b92da049d45b27b39ecc4e2c7
Aijan19 <kelgenbaevaajzana@gmail.com>
2023-11-14 20:59:11 +0600

    Все что успела :(

---
+++ main.js
@@ -0,0 +1,79 @@
+//! Задание 1
+
+const a = 73
+    b = 'Proficent'
+    c = true
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+//! Задание 2
+
+console.log(typeof + '66')
+console.log(typeof typeof 23)
+
+//! Задание 3
+
+let arr = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < arr.length; i++) {
+   console.log(arr[i]); 
+}
+
+
+//! Задание 4
+
+const obj = {
+    name: 'Aijan',
+    age: 21,
+    hometown: 'Karakol',
+    profession: 'Journalist'
+}
+
+console.log(obj)
+
+//! Задание 5
+
+// const userInput = prompt ("Введите число");
+
+// if(userInput % 2 === 0) {
+//     console.log('Число четное');
+// } else {
+//     console.log('Число не четное');
+// }
+
+//! Задание 6
+
+const Arr = [1,2,3,4,5,6,7,8,9,10]
+
+for(let item of Arr) {
+    console.log(item)
+}
+
+//! Задание 7
+
+function Word (d, e) {
+    console.log(d + e)
+}
+
+Word('Hello ', 'World!')
+
+const arrowFunc = () => {
+    console.log("Hello World!");
+};
+
+arrowFunc();
+
+function declarationFunc() {
+    console.log("Hello World!");
+}
+
+declarationFunc();
+
+//! Задание 8
+
+const myPromise = new Promise((resolve, reject) => {
+    
+  });
+  
